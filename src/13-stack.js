aeb20ceb625c52cb477a2f46f31dcd543cdcfafa
wukapHo <xb0ksx@gmail.com>
2021-04-11 11:53:29 +0300

    feat: complete stack

--- src/13-stack.js
+++ src/13-stack.js
@@ -11,16 +11,20 @@
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new Error('Not implemented');
+  constructor() {
+    this.arr = [];
+  }
+
+  push(element) {
+    return this.arr.push(element);
   }
 
   pop() {
-    throw new Error('Not implemented');
+    return this.arr.pop();
   }
 
   peek() {
-    throw new Error('Not implemented');
+    return this.arr[this.arr.length - 1];
   }
 }
 
